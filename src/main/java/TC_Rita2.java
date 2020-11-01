b7bdb476eacaf71880c240364d21591de7df5e55
RitaFurman <margaret_furman@yahoo.com>
2020-11-01 15:01:45 -0600

    new test case

---
+++ src/main/java/TC_Rita2.java
@@ -0,0 +1,7 @@
+import javax.imageio.stream.ImageInputStream;
+
+public class TC_Rita2 {
+    public static void main(String[] args) {
+        System.out.println("working on new test case2");
+    }
+}
