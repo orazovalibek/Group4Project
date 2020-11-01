f9a2aee2adfc30378e4a59e9d2919e6d3289a996
orazovalibek <orazalike@gmail.com>
2020-11-01 14:42:13 -0500

    initial commit

---
+++ src/main/java/GreatGroup4.java
@@ -0,0 +1,5 @@
+public class GreatGroup4 {
+    public static void main(String[] args) {
+        System.out.println("Every student from this awesome group will get their dream job offers soon");
+    }
+}
