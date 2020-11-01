c94b043d9b6029d2410ad0828bf36e7da025684a
johnbasi2020 <basilos2005@gmail.com>
2020-11-01 14:34:22 -0600

    the way to work

--- src/main/java/John2020.java
+++ src/main/java/John2020.java
@@ -2,5 +2,7 @@ public class John2020 {
 
     public static void main(String[] args) {
         System.out.println("this is the best way to learn");
+
+        //hope one day my dream come true
     }
 }
