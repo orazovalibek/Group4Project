9060350846ce187b86187c192d7146b50611b5da
RitaFurman <margaret_furman@yahoo.com>
2020-11-01 14:51:20 -0600

    new note added

--- src/main/java/TC_RitaHere.java
+++ src/main/java/TC_RitaHere.java
@@ -1,5 +1,6 @@
 public class TC_RitaHere {
     public static void main(String[] args) {
         System.out.println("I'm working on my test case");
+        //test, test...new code lines are coming
     }
 }
