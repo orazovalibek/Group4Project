c303ddbfed9cf5ca5bbf45c432ef4d129e763926
orazovalibek <orazalike@gmail.com>
2020-11-01 15:24:16 -0500

    adding SecretClass

--- src/main/java/GreatGroup4.java
+++ src/main/java/GreatGroup4.java
@@ -1,5 +1,7 @@
 public class GreatGroup4 {
+
     public static void main(String[] args) {
         System.out.println("Every student from this awesome group will get their dream job offers soon");
     }
+
 }
---
+++ src/test/java/SecretClass.java
@@ -0,0 +1,5 @@
+public class SecretClass {
+    public static void main(String[] args) {
+
+    }
+}
