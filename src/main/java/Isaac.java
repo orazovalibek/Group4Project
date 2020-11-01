b76997784445a47df627a696f7f8056a0c04703a
Arasefe <ismail_yildirim69@yahoo.com>
2020-11-01 14:56:16 -0600

    Sixth commit

--- src/main/java/Isaac.java
+++ src/main/java/Isaac.java
@@ -1,16 +1,34 @@
 public class Isaac {
     public static void main(String[] args) {
         System.out.println("This is how Git and Github works when team members collaborate");
-
+        successMethod();
 
     }
 
     public static void successMethod(){
-        boolean success=false;
 
-        int collaboration=(success)?100:(!success)?1:0;
+        int dailyCollaboration=1;
+        int successRate=0;
 
-        System.out.println(collaboration);
+        switch (dailyCollaboration){
+            case 1:
+                successRate=10;
+                break;
+            case 2:
+                successRate=40;
+                break;
+            case 3:
+                successRate=60;
+                break;
+            case 4:
+                successRate=80;
+                break;
+            case 5:
+                successRate=100;
+                break;
+            default:
+                successRate=-1;
+        }
     }
 
 
