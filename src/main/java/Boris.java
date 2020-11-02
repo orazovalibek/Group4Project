cc349d150bb2dad1ee471fff18379cf8d458687c
borusik789 <borusik789@gmail.com>
2020-11-02 16:30:04 +0300

    Changes

--- src/main/java/Boris.java
+++ src/main/java/Boris.java
@@ -1,6 +1,7 @@
 public class Boris {
     public static void main(String[] args) {
-        System.out.println("Boris is working in Amazon and Google");
+
+        System.out.println("Boris is working in Amazon and Google1");
     }
 
 }
